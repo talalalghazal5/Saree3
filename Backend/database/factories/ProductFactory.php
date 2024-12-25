cfa95ae5cc1c00622f8bf6b8f4763ef7d107bd1c
Obaida Almelhem <almelhemo@gmail.com>
2024-12-25 00:18:42 -0800

    update Product factory

--- Backend/database/factories/ProductFactory.php
+++ Backend/database/factories/ProductFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as FakerFactory;
+use \Bezhanov\Faker\Provider\Commerce;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -16,8 +18,18 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+
+        $faker = FakerFactory::create();
+        $faker->addProvider(new Commerce($faker));
+
         return [
-            //
+            'name' => $faker->productName,
+            'description' => $this->faker->paragraph,
+            'rating' => $this->faker->numberBetween(0, 5),
+            'price' => $this->faker->numberBetween(100, 10000),
+            'stock_quantity' => $this->faker->numberBetween(0, 100),
+            'total_rating' => $this->faker->numberBetween(0, 1000),
+            'total_review_count' => $this->faker->numberBetween(0, 500),
         ];
     }
 }
