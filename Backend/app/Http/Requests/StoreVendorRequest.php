e8bf947401a785add8e0a493b3927c83b2695967
Obaida Almelhem <almelhemo@gmail.com>
2024-12-27 17:36:12 +0300

    added store & update Vendor form requests

---
+++ Backend/app/Http/Requests/StoreVendorRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreVendorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'total_rating' => 'required|integer|min:0|max:500',
+            'contact_number' => 'nullable|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255',
+        ];
+    }
+}
