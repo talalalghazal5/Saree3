a07122e4736a4fb7baaa6592483a8fa2ff723bc6
Obaida Almelhem <almelhemo@gmail.com>
2024-12-27 16:28:57 +0300

    update Vendor Factory

--- Backend/database/factories/VendorFactory.php
+++ Backend/database/factories/VendorFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class VendorFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'name' => $this->faker->company,
+            'description' => $this->faker->paragraph,
+            'total_rating' => $this->faker->numberBetween(1,500), //todo update with helper function
+            'contact_number' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'email' => $this->faker->companyEmail,
         ];
     }
 }
