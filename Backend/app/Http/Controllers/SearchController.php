89746c7f5d3dfc64b5d2e94e3b0e49a4e76d636a
Obaida Almelhem <almelhemo@gmail.com>
2024-12-28 00:51:55 +0300

    adding search controller and endpoint

---
+++ Backend/app/Http/Controllers/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Vendor;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $query = $request->input('searchQuery');
+
+        // Search products
+        $products = Product::where('name', 'like', "%{$query}%")
+            ->orWhere('description', 'like', "%{$query}%")
+            ->get(['id', 'name', 'description', 'price', 'total_rating as rating']);
+
+        // Search vendors
+        $vendors = Vendor::where('name', 'like', "%{$query}%")
+            ->orWhere('description', 'like', "%{$query}%")
+            ->get(['id', 'name', 'description', 'total_rating as rating']);
+
+        // Search categories
+        $categories = Category::where('name', 'like', "%{$query}%")
+            ->orWhere('description', 'like', "%{$query}%")
+            ->get(['id', 'name', 'description']);
+
+        return response()->json([
+            'searchResult' => [
+                'products' => $products,
+                'vendors' => $vendors,
+                'categories' => $categories,
+            ]
+        ]);
+    }
+}
