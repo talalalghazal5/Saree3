399fbc7e17007bcea1b49ff0e150ed619a54e16f
Obaida Almelhem <almelhemo@gmail.com>
2024-12-27 22:48:39 +0300

    adding Vendor relationship to User Model

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function vendor()
+    {
+        return $this->hasOne(Vendor::class);
+    }
 }
