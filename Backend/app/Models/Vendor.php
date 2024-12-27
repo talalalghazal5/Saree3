60570f99f365ecf91dbb2c0d0544a7226ca46d8d
Obaida Almelhem <almelhemo@gmail.com>
2024-12-27 16:28:13 +0300

    update Vendor model

--- Backend/app/Models/Vendor.php
+++ Backend/app/Models/Vendor.php
@@ -9,4 +9,24 @@ class Vendor extends Model
 {
     /** @use HasFactory<\Database\Factories\VendorFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'total_rating',
+        'contact_number',
+        'address',
+        'email',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
