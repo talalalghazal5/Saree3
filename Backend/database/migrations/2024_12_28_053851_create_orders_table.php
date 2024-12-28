e3c3069fd53f75bbce676bac45874263d8e15561
Obaida Almelhem <almelhemo@gmail.com>
2024-12-28 08:47:28 +0300

    update Order migrations

--- Backend/database/migrations/2024_12_28_053851_create_orders_table.php
+++ Backend/database/migrations/2024_12_28_053851_create_orders_table.php
@@ -1,27 +1,31 @@
-<?php
+ <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('orders', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+                $table->unsignedInteger('total_price');
+                $table->enum('status', ['pending', 'completed', 'canceled'])->default('pending');
+                $table->timestamp('closed_date')->nullable();
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('orders');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('orders');
+        }
+    };
