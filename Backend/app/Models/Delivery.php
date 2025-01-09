2298385774b2f4c5afb1a72581cc9830f42de340
Obaida Almelhem <almelhemo@gmail.com>
2025-01-10 00:55:21 +0300

    update Delivery model

--- Backend/app/Models/Delivery.php
+++ Backend/app/Models/Delivery.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Delivery extends Model
 {
-    //
+    protected $fillable = [
+        'order_id',
+        'name',
+        'phone_number',
+        'expected_delivery_time',
+        'state'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
