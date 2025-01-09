be9ec2dfbe74ba0483f739f79edcbda1c00d2f9c
Obaida Almelhem <almelhemo@gmail.com>
2025-01-10 00:56:01 +0300

    update Order model to include Delivery relationship

--- Backend/app/Models/Order.php
+++ Backend/app/Models/Order.php
@@ -25,4 +25,9 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function delivery()
+    {
+        return $this->hasOne(Delivery::class);
+    }
 }
