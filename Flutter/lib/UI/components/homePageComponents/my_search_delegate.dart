9a316288b891ed66acb9bfaa4727a7916c42bd3c
talalcodes55 <talalalghazal55@gmail.com>
2025-01-11 00:28:24 +0300

    styled results of vendors and categories

--- Flutter/lib/UI/components/homePageComponents/my_search_delegate.dart
+++ Flutter/lib/UI/components/homePageComponents/my_search_delegate.dart
@@ -81,8 +81,9 @@ class MySearchDelegate extends SearchDelegate {
                     onTap: () => Navigator.pushReplacement(
                       context,
                       CupertinoPageRoute(
-                        builder: (context) =>
-                            ProductDetailPage(product: products[index]),
+                        builder: (context) => ProductDetailPage(
+                          product: products[index],
+                        ),
                       ),
                     ),
                   );
@@ -98,34 +99,39 @@ class MySearchDelegate extends SearchDelegate {
                 .copyWith(fontWeight: FontWeight.bold),
           ),
           Expanded(
-              child: FutureBuilder(
-            future: HomePageService().search(query),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
+            child: FutureBuilder(
+              future: HomePageService().search(query),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('No data found'),
+                  );
+                }
+                var searchResult = snapshot.data!;
+                List<Vendor> vendors = searchResult['vendors'];
+                if (vendors.isEmpty) {
+                  return const Center(
+                    child: Text('no data found'),
+                  );
+                }
+                return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: vendors.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(vendors[index].name!),
+                      subtitle:
+                          Text('Contact: ${vendors[index].contactNumber}'),
+                    );
+                  },
                 );
-              } else if (snapshot.data!.isEmpty) {
-                return const Center(
-                  child: Text('No data found'),
-                );
-              }
-              var searchResult = snapshot.data!;
-              List<Vendor> vendors = searchResult['vendors'];
-              if (vendors.isEmpty) {
-                return const Center(
-                  child: Text('no data found'),
-                );
-              }
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: vendors.length,
-                itemBuilder: (context, index) {
-                  return Text(vendors[index].name!);
-                },
-              );
-            },
-          )),
+              },
+            ),
+          ),
           Text(
             'Categories:',
             style: Theme.of(context)
@@ -134,34 +140,38 @@ class MySearchDelegate extends SearchDelegate {
                 .copyWith(fontWeight: FontWeight.bold),
           ),
           Expanded(
-              child: FutureBuilder(
-            future: HomePageService().search(query),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
+            child: FutureBuilder(
+              future: HomePageService().search(query),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('No data found'),
+                  );
+                }
+                var searchResult = snapshot.data!;
+                List<Category> categories = searchResult['categories'];
+                if (categories.isEmpty) {
+                  return const Center(
+                    child: Text('no data found'),
+                  );
+                }
+                return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return FilterChip(
+                      label: Text(categories[index].name!),
+                      onSelected: (value) {},
+                    );
+                  },
                 );
-              } else if (snapshot.data!.isEmpty) {
-                return const Center(
-                  child: Text('No data found'),
-                );
-              }
-              var searchResult = snapshot.data!;
-              List<Category> categories = searchResult['categories'];
-              if (categories.isEmpty) {
-                return const Center(
-                  child: Text('no data found'),
-                );
-              }
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return Text(categories[index].name!);
-                },
-              );
-            },
-          )),
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -169,8 +179,23 @@ class MySearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Center(
-      child: Text('Search for products, vendors...'),
+    return Center(
+      child: Text(
+        'Search for products, vendors...',
+        style: Theme.of(context)
+            .textTheme
+            .bodyMedium!
+            .copyWith(color: Theme.of(context).colorScheme.inverseSurface),
+      ),
     );
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return theme.copyWith(
+        inputDecorationTheme: InputDecorationTheme(
+            border: const UnderlineInputBorder(borderSide: BorderSide.none),
+            hintStyle: Theme.of(context).textTheme.bodyLarge));
   }
 }
