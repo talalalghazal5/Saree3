217eb748391005082e8520856f9e9650bd4eb6a3
talalcodes55 <talalalghazal55@gmail.com>
2025-01-18 19:42:58 +0300

    some edits on the order details page

--- Flutter/lib/UI/pages/order_details.dart
+++ Flutter/lib/UI/pages/order_details.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:saree3/controllers/cart_provider.dart';
+import 'package:saree3/controllers/user_controller.dart';
 import 'package:saree3/data/models/cart_item.dart';
 import 'package:saree3/services/order_service.dart';
 import 'package:intl/intl.dart';
 
 class OrderDetails extends StatefulWidget {
   const OrderDetails({super.key});
-
   @override
   State<OrderDetails> createState() => _OrderDetailsState();
 }
@@ -15,7 +15,9 @@ class OrderDetails extends StatefulWidget {
 class _OrderDetailsState extends State<OrderDetails> {
   @override
   Widget build(BuildContext context) {
-    CartProvider cartProvider = Provider.of<CartProvider>(context);
+    // CartProvider cartProvider = Provider.of<CartProvider>(context);
+    UserController userController =
+        Provider.of<UserController>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -30,6 +32,16 @@ class _OrderDetailsState extends State<OrderDetails> {
               future: OrderService()
                   .placeNewOrder(Provider.of<CartProvider>(context).cart),
               builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ],
+                  );
+                }
                 if (snapshot.hasData) {
                   String formattedDate = DateFormat('EEE, d/m/y - HH:mm')
                       .format(snapshot.data!.orderDate!);
@@ -57,74 +69,14 @@ class _OrderDetailsState extends State<OrderDetails> {
                           const SizedBox(
                             height: 5,
                           ),
+                          const SizedBox(
+                            height: 5,
+                          ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text('User ID:'),
-                              Text(
-                                '${snapshot.data!.userId}',
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .inverseSurface),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Divider(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .inverseSurface
-                                .withAlpha(60),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Text('Delivery Address'),
-                              Text('As Suwayda, Syria'),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Divider(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .inverseSurface
-                                .withAlpha(60),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('Order Status'),
-                              Text('Pending'),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Divider(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .inverseSurface
-                                .withAlpha(60),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('Total Cost:'),
-                              Text('\$1999.5'),
+                              Text(userController.user.location!),
                             ],
                           ),
                           const SizedBox(
@@ -142,45 +94,107 @@ class _OrderDetailsState extends State<OrderDetails> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text('Ordered At'),
-                              Text(DateFormat('EEE, d/m/y - HH:mm')
-                                  .format(DateTime.now())),
+                              Text('Order Status:'),
+                              Text(snapshot.data!.orderStatus),
                             ],
                           ),
-                          const SizedBox(height: 20,),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Divider(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .inverseSurface
+                                .withAlpha(60),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Total Cost:'),
+                              Text('\$${snapshot.data!.totalPrice}'),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Divider(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .inverseSurface
+                                .withAlpha(60),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Ordered A:'),
+                              Text(DateFormat('EEE, d/M/y - HH:mm')
+                                  .format(snapshot.data!.orderDate!)),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
                           SizedBox(
-                            height: 450,
+                              height: 450,
                               child: ListView.builder(
-                            itemCount: cartProvider.cart.length,
-                            itemBuilder: (context, index) {
-                              CartItem cartItem = cartProvider.cart[index];
-                              return ListTile(
-                                contentPadding: const EdgeInsets.only(left: 5, right: 10, bottom: 5, top: 5),
-                                tileColor: Theme.of(context).colorScheme.inverseSurface.withAlpha(30),
-                                shape: const BeveledRectangleBorder(side: BorderSide(width: 2, color: Colors.black), borderRadius: BorderRadius.all(Radius.circular(10))),
-                                leading: Image.network(
-                                  cartItem.product.image!,
-                                  width: 50,
-                                ),
-                                title: Text(
-                                  cartItem.product.name!,
-                                  style: Theme.of(context).textTheme.bodySmall,
-                                ),
-                                subtitle: Text(
-                                  'Price: ${cartItem.product.price}',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelSmall!
-                                      .copyWith(
+                                itemCount: snapshot.data!.orderItems!.length,
+                                itemBuilder: (context, index) {
+                                  // CartItem cartItem = cartProvider.cart[index];
+                                  return ListTile(
+                                    contentPadding: const EdgeInsets.only(
+                                      left: 5,
+                                      right: 10,
+                                      bottom: 5,
+                                      top: 5,
+                                    ),
+                                    tileColor: Theme.of(context)
+                                        .colorScheme
+                                        .inverseSurface
+                                        .withAlpha(30),
+                                    shape: RoundedRectangleBorder(
+                                      side: BorderSide(
+                                          width: 1,
                                           color: Theme.of(context)
                                               .colorScheme
-                                              .inverseSurface),
-                                ),
-                                trailing:
-                                    Text('QTY: ${cartItem.quantity}', style: const TextStyle(fontSize: 13),),
-                              );
-                            },
-                          ))
+                                              .onSurface),
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      ),
+                                    ),
+                                    leading: Image.network(
+                                      snapshot.data!.orderItems![index].product
+                                          .image!,
+                                      width: 50,
+                                    ),
+                                    title: Text(
+                                      snapshot.data!.orderItems![index].product
+                                          .name!,
+                                      style:
+                                          Theme.of(context).textTheme.bodySmall,
+                                    ),
+                                    subtitle: Text(
+                                      'Price: ${snapshot.data!.orderItems![index].product.price}',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelSmall!
+                                          .copyWith(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .inverseSurface),
+                                    ),
+                                    trailing: Text(
+                                      'QTY: ${snapshot.data!.orderItems![index].quantity}',
+                                      style: const TextStyle(fontSize: 13),
+                                    ),
+                                  );
+                                },
+                              ))
                         ],
                       ),
                     ),
@@ -190,15 +204,27 @@ class _OrderDetailsState extends State<OrderDetails> {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Center(
-                        child: Text(snapshot.error.toString()),
-                      )
+                      Text('Error Occured'),
+                      MaterialButton(
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        child: Text(
+                          'Try again',
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelSmall!
+                              .copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inverseSurface),
+                        ),
+                      ),
                     ],
                   );
                 }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
+
+                return Container();
               },
             )
           ],
