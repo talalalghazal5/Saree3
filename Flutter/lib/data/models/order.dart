8df012f2701ae9d2017a659b396fb91f2b558c46
talalcodes55 <talalalghazal55@gmail.com>
2025-01-18 19:37:50 +0300

    added the order items to the jsfying function of the order model

--- Flutter/lib/data/models/order.dart
+++ Flutter/lib/data/models/order.dart
@@ -1,19 +1,24 @@
+import 'package:saree3/data/models/cart_item.dart';
+
 class Order {
   int id;
   int userId;
   double totalPrice;
   String orderStatus;
   DateTime? orderDate;
+  List<CartItem>? orderItems;
 
-  Order({this.id = 0, this.userId = 0, this.totalPrice = 0.0, this.orderStatus = 'pending', this.orderDate});
+  Order({this.id = 0, this.userId = 0, this.totalPrice = 0.0, this.orderStatus = 'pending', this.orderDate, this.orderItems});
 
   factory Order.fromJson(Map<String, dynamic> json) {
     return Order(
+      
       id: json['id'],
       userId: json['userId'],
       totalPrice: json['totalPrice'],
       orderStatus: json['orderStatus'],
       orderDate: DateTime.parse(json['createdAt'] as String),
+      orderItems: (json['orderItems'] as List<dynamic>).map((item) => CartItem.fromJson(item as Map<String, dynamic>)).toList(),
     );
   }
 
@@ -24,7 +29,7 @@ class Order {
     data['totalPrice'] = totalPrice;
     data['orderStatus'] = orderStatus;
     data['orderDate'] = orderDate;
-
+    data['orderItems'] = orderItems!.map((item) => item.toJson()).toList();
     return data;
   }
 }
